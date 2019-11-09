28494b0cfa6994638ee2e4d75231b7fec95a4c82
Alex Budyakov <a.budyakov@kdd-digital.de>
2019-11-09 15:26:51 +0200

    wip

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import logo from '../../static/images/logo.svg';
+import logoModified from '../../static/images/logo_modified.svg';
 
 const LogoContainer = styled.a`
 	font-size: 1.8rem;
@@ -12,10 +13,25 @@ const LogoContainer = styled.a`
 	&:focus {
 		color: rgba(0, 0, 0, 0.9);
 	}
+	&:hover img:first-of-type {
+		display: none;
+	}
+	&:hover img:last-of-type {
+		display: block;
+	}
+`;
+
+const DefaultImage = styled.img`
+	display: block;
+`;
+
+const ModifiedImage = styled.img`
+	display: none;
 `;
 
 export default () => (
 	<LogoContainer href="/">
-		<img src={logo} alt="BuildApps" />
+		<DefaultImage src={logo} alt="BuildApps" />
+		<ModifiedImage src={logoModified} alt="BuildApps" />
 	</LogoContainer>
 );
