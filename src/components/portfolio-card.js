2268ab21fd991efd5912dde73b592162c7d744f9
ivan.kolosinskyi <xoikolos@zess.com>
2019-11-09 10:23:28 +0200

    Add portfolio triangle

--- src/components/portfolio-card.js
+++ src/components/portfolio-card.js
@@ -3,65 +3,80 @@ import styled from 'styled-components';
 import { theme } from '../styles/theme';
 
 const CardContainer = styled.div`
-    display: flex;
-    width:100%;
-    height: auto;
-    flex-direction: column;
-    margin: 1px 9px;
+	display: flex;
+	width: 100%;
+	height: auto;
+	flex-direction: column;
+	margin: 1px 9px;
+	border: 1px solid ${theme.colors.black};
 `;
 
 const HeaderImage = styled.img`
-    border-width: 1px;
-    border-color: ${theme.colors.black};
-    border-style: solid;
-    object-fit: fill;
-    width: 100%;
-    padding: 6%;
+	object-fit: fill;
+	width: 100%;
+	padding: 6%;
     height: 187px;
+    z-index: -2;
+	border-bottom: 1px solid ${theme.colors.black};
 `;
 
 const InformationContainer = styled.div`
-    color: ${theme.colors.black};
-    border-width: 0 1px 1px 1px;
-    border-color: ${theme.colors.black};
-    bacground-color: ${theme.colors.black};
-    border-style: solid;
-    height: 117px;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    display: flex;
-        &:hover {
-            background: #8147A5;
-            color: #ffffff;
-        }
-        &:hover div:first-of-type {
-            color: white;
-        }
-        &:hover div:last-of-type {
-            display: block;
-        }
+	position: relative;
+	height: 117px;
+	color: ${theme.colors.black};
+	background: ${theme.colors.white};
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+
+	&:before {
+		content: '';
+		display: block;
+		position: absolute;
+		width: 20px;
+		height: 20px;
+		background: ${theme.colors.white};
+		transform: rotate(45deg);
+		border: 1px solid ${theme.colors.black};
+		border-width: 1px 0 0 1px;
+		top: -10px;
+		left: calc(50% - 10px);
+		z-index: -1;
+	}
+	&:hover,
+	&:hover:before {
+		background: #8147a5;
+		color: #ffffff;
+	}
+	&:hover div:first-of-type {
+		color: ${theme.colors.white};
+	}
+	&:hover div:last-of-type {
+		display: block;
+	}
 `;
 
 const Title = styled.div`
-    padding: 2%;
-    font-size: 1.3rem;
-    text-align: center;
+	padding: 2%;
+	font-size: 1.3rem;
+	text-align: center;
 `;
 
 const Description = styled.div`
-    padding: 2%;
-    font-size: 0.8rem;
-    color: white;
-    display: none;
-    text-align: center;
+	padding: 2%;
+	font-size: 0.8rem;
+	color: ${theme.colors.white};
+	display: none;
+	text-align: center;
 `;
 
-export default ({ image, text, description }) =>
-    <CardContainer>
-        <HeaderImage src={image} />
-        <InformationContainer>
-            <Title>{text}</Title>
-            <Description>{description}</Description>
-        </InformationContainer>
-    </CardContainer>
+export default ({ image, text, description }) => (
+	<CardContainer>
+		<HeaderImage src={image} />
+		<InformationContainer>
+			<Title>{text}</Title>
+			<Description>{description}</Description>
+		</InformationContainer>
+	</CardContainer>
+);
