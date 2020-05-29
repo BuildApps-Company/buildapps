2b1d05eefa24b02693a2be3feaa0514800265812
Ivan Kolosinskyi <ivan.kolosinskyi.ext@zeiss.com>
2020-05-29 17:03:32 +0300

    Update google analytics position

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,9 +13,9 @@ module.exports = {
 			options: {
 				trackingId: process.env.googleTrackId,
 				// Defines where to place the tracking script - `true` in the head and `false` in the body
-				head: false,
+				head: true,
 				// Setting this parameter is optional
-				anonymize: true,
+				// anonymize: true,
 				// Setting this parameter is also optional
 				respectDNT: true,
 				// Avoids sending pageview hits from custom paths
