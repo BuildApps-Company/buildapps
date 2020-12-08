e6c2a7f93abf4e7fad5163f5100fa6b5d9d2cfa4
Vyacheslav <slavikeptask@gmail.com>
2020-12-08 17:46:57 +0300

    init

---
+++ src/pages/PortfolioDetailsPage/PortfolioDetailsPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PortfolioDetailsPage() {
+	return (
+		<>
+			<h1>Portfolio details</h1>
+			<a href="/MainPage/MainPage/">Goto main</a>
+		</>
+	);
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,17 @@
 import React from 'react';
-import Layout from '../components/layout';
-import Preview from '../components/preview';
-import ContactForm from '../components/contact-form';
-import Portfolio from '../components/portfolio';
-import Services from '../components/services';
-import Team from '../components/team';
-import { Helmet } from 'react-helmet';
+import {Helmet} from 'react-helmet'
+import MainPage from './MainPage/MainPage'
 
 export default () => (
-	<>
-		<Helmet>
-			<title>BuildApps | Software development agency | UI/UX Design</title>
-			<meta
-				name="description"
-				content="We specialize in developing applications for various platforms and services. BuildApps creates mobile, websites, desktop applications using the of best approaches."
-			/>
-			<meta http-equiv="Content-Language" content="en"></meta>
-		</Helmet>
-		<Layout>
-			<Preview />
-			<Services />
-			<Portfolio />
-			<Team />
-			<ContactForm />
-		</Layout>
-	</>
-);
+    <>
+	<Helmet>
+		<title>BuildApps | Software development agency | UI/UX Design</title>
+		<meta
+			name="description"
+			content="We specialize in developing applications for various platforms and services. BuildApps creates mobile, websites, desktop applications using the of best approaches."
+		/>
+		<meta http-equiv="Content-Language" content="en"></meta>
+	</Helmet>
+    <MainPage/>
+    </>
+)
