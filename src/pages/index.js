67f6fa1482be66ff4566c36cc737e77bfbe3cd01
Алена Коломиец <alenakolomiec@MacBook-Air-Alena.local>
2021-01-24 15:32:44 +0200

    added slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,9 @@ import Preview from '../components/preview';
 import ContactForm from '../components/contact-form';
 import Portfolio from '../components/portfolio';
 import Services from '../components/services';
-import Team from '../components/team';
 import { Helmet } from 'react-helmet';
+// import Team from '../components/team';
+
 
 export default () => (
 	<>
@@ -14,14 +15,17 @@ export default () => (
 			<meta
 				name="description"
 				content="We specialize in developing applications for various platforms and services. BuildApps creates mobile, websites, desktop applications using the of best approaches."
+
 			/>
 			<meta http-equiv="Content-Language" content="en"></meta>
+			<link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
 		</Helmet>
 		<Layout>
 			<Preview />
 			<Services />
 			<Portfolio />
-			<Team />
+			{/* <Team /> */}
 			<ContactForm />
 		</Layout>
 	</>
