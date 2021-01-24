4b82d7f6e03f7deed7805cb1b9d36f4e77a5c1ee
Ivan Kolosinskyi <ivan.kolosinskyi@globallogic.com>
2021-01-24 17:51:44 +0200

    Repear mobile layout

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -2,81 +2,26 @@ import React from 'react';
 import { Component } from 'react';
 import styled from 'styled-components';
 import PortfolioCard from './portfolio-card';
-import SectionHeader from './section-header';
+import SectionHeader, { Divider } from './section-header';
 import { Menu } from '../data/menu';
-import { SlideContainer } from '../styles/shared';
 import { Portfolio } from '../data/portfolio';
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 import { theme } from '../styles/theme';
 
-// import Slider from 'react-slick';
-// import 'react-slick/slick-carousel/slick/slick.css';
-// import 'react-slick/slick-carousel/slick/slick-theme.css';
-
-// const CardsContainer = styled(SlideContainer)`
-// 	display: flex;
-// 	justify-content: center;
-// 	flex-wrap: wrap;
-// `;
-
-// export default () => (
-// 	<div id={Menu.portfolio.key}>
-// 		<SectionHeader
-// 			smallDescription="portfolio"
-// 			largeDescription="Projects we worked on"
-// 		/>
-// 		<CardsContainer>
-// 			{Object.entries(Portfolio).map(([key, example]) => (
-// 				<PortfolioCard
-// 					key={key}
-// 					text={example.title}
-// 					image={example.image}
-// 					description={example.description}
-// 				/>
-// 			))}
-// 		</CardsContainer>
-// 	</div>
-// );
-
-// export default class SimpleSlider extends Component {
-// 	render() {
-// 		const settings = {
-// 			dots: false,
-// 			infinite: true,
-// 			speed: 500,
-// 			arrows: true,
-// 			slidesToShow: 1,
-// 			slidesToScroll: 1,
-
-// 		};
-// 		return (
-// 			<div id={Menu.portfolio.key}>
-// 				<SectionHeader
-// 					smallDescription="portfolio"
-// 					largeDescription="Projects we worked on"
-// 				/>
-// 				<Slider {...settings}>
-// 					{Object.entries(Portfolio).map(([key, example]) => (
-// 						<PortfolioCard
-// 							key={key}
-// 							text={example.title}
-// 							image={example.image}
-// 							description={example.description}
-// 						/>
-// 					))}
-// 				</Slider>
-// 			</div>
-// 		);
-// 	}
-// }
 const StyledAwesomeSlider = styled(AwesomeSlider)`
+	--slider-height-percentage: 250px !important;
+	padding-bottom: 15vh;
+
+	@media all and (max-width: ${theme.breakpoints.tablet}px) {
+		--slider-height-percentage: 350px !important;
+	}
+
 	.awssld__content {
 		background-color: ${theme.colors.primary};
 	}
 
 	.awssld__wrapper {
-		height: 48vh;
 		max-width: 1200px;
 		margin: 0 auto;
 	}
@@ -95,6 +40,10 @@ const Container = styled.div`
 
 const StyledSectionHeader = styled(SectionHeader)`
 	color: ${theme.colors.whiteSmoke};
+
+	${Divider} {
+		margin-bottom: 0;
+	}
 `;
 
 export default () => (
