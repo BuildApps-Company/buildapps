62df3d4e92fee143018d7eef8bdfac3fc7d82334
Алена Коломиец <alenakolomiec@MacBook-Air-Alena.local>
2021-07-14 14:10:15 +0300

    changed burger

--- src/components/StyledBurger/StyledBurger.jsx
+++ src/components/StyledBurger/StyledBurger.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const StyledBurger = ({ toogleList, active }) => {
+const StyledBurger = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const toogleList = () => setIsOpen(prevState => !prevState);
 	return (
 		<>
 			<SvgWrap>
@@ -22,11 +24,11 @@ const StyledBurger = ({ toogleList, active }) => {
 					</filter>
 				</defs>
 			</SvgWrap>
-			<Plate onClick={toogleList} active={active}>
+			<Plate onClick={toogleList} active={isOpen}>
 				<Burger
-					width="64"
-					height="64"
-					viewBox="0 0 64 64"
+					width="100"
+					height="100"
+					viewBox="0 0 100 100"
 					fill="none"
 					xmlns="http://www.w3.org/2000/svg"
 				>
@@ -35,19 +37,10 @@ const StyledBurger = ({ toogleList, active }) => {
 					<Line3 d="M 50,50 H 70 L 66,68" />
 					<Line4 d="M 30,65 H 70 " />
 				</Burger>
-				<X
-					width="40"
-					height="40"
-					viewBox="0 0 40 40"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<Line
-						fill-rule="evenodd"
-						clip-rule="evenodd"
-						d="M12.9461 12.1623C12.7297 11.9459 12.3788 11.9459 12.1623 12.1623C11.9459 12.3788 11.9459 12.7297 12.1623 12.9461L19.2165 20.0002L12.1625 27.0542C11.9461 27.2706 11.9461 27.6215 12.1625 27.8379C12.379 28.0544 12.7299 28.0544 12.9463 27.8379L20.0002 20.784L27.0539 27.8377C27.2703 28.0541 27.6212 28.0541 27.8377 27.8377C28.0541 27.6212 28.0541 27.2703 27.8377 27.0539L20.784 20.0002L27.8379 12.9464C28.0543 12.7299 28.0543 12.379 27.8379 12.1626C27.6215 11.9462 27.2706 11.9462 27.0541 12.1626L20.0002 19.2165L12.9461 12.1623Z"
-						fill="#ffffff"
-					/>
+
+				<X version="1.1" height="100" width="100" viewBox="0 0 100 100">
+					<Line d="M 34,32 L 66,68" />
+					<Line d="M 66,32 L 34,68" />
 				</X>
 			</Plate>
 		</>
@@ -63,8 +56,8 @@ const SvgWrap = styled.svg`
 `;
 const Line = styled.path`
 	fill: none;
-	stroke: white;
-	stroke-width: 4px;
+	stroke: black;
+	stroke-width: 6px;
 	stroke-linecap: round;
 	stroke-linejoin: round;
 	transform-origin: 50%;
@@ -85,7 +78,10 @@ const Line4 = styled(Line)`
 	stroke-dasharray: 40 40;
 `;
 
-const X = styled(SvgWrap)`
+const X = styled.svg`
+	height: 80px;
+	position: absolute;
+	width: 80px;
 	transform: scale(0);
 	transition: transform 400ms;
 
@@ -95,31 +91,26 @@ const X = styled(SvgWrap)`
 `;
 
 const Plate = styled.div`
-	height: 80px;
-	width: 80px;
-	${Line} {
-		transition: stroke-dasharray 400ms 100ms, stroke-dashoffset 400ms 100ms,
-			transform 400ms 100ms;
-	}
-	${Line1} 
-	${Line2} 
-	${Line3} 
-	${Line4} 
-	${X} {
-		transition: transform 400ms 50ms;
-	}
-	${({ active }) =>
+  height: 80px;
+  width: 80px;
+  ${Line} {
+    transition: stroke-dasharray 400ms 100ms, stroke-dashoffset 400ms 100ms,
+      transform 400ms 100ms;
+  }
+
+  ${X} {
+    transition: transform 400ms 50ms;
+  }
+  ${({ active }) =>
 		active &&
 		`${Line} {transition: stroke-dasharray 500ms, stroke-dashoffset 500ms, transform 500ms;}`}
-		
 
-${({ active }) =>
-	active && `${X} {transform: scale(1); transition: transform 400ms 350ms;}`}
+  ${({ active }) =>
+		active && `${X} {transform: scale(1); transition: transform 400ms 350ms;}`}
 
 		${({ active }) =>
 			active &&
 			`${Line} { transition: stroke-dasharray 400ms, stroke-dashoffset 400ms, transform 400ms;}`}
-			
 
 	${({ active }) =>
 		active &&
@@ -142,8 +133,10 @@ ${({ active }) =>
   stroke-dashoffset: -33px;}`}
 
 ${({ active }) => active && `${X} {transition: transform 400ms 50ms;}`}
-
 `;
-const Burger = styled(SvgWrap)`
+const Burger = styled.svg`
+	height: 80px;
+	position: absolute;
+	width: 80px;
 	filter: url(#gooeyness);
 `;
