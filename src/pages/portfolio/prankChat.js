70adabd93c2082a077a96256916b3dc6531fe4eb
Алена Коломиец <alenakolomiec@MacBook-Air-Alena.local>
2021-07-14 19:57:06 +0300

    deleted prankchat

--- src/pages/portfolio/prankChat.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-// import { PortfolioDetails } from '../../components/PortfolioPage/index';
-
-// const prankChat = () => {
-// 	return <PortfolioDetails id={'prankChat'}></PortfolioDetails>;
-// };
-
-// export default prankChat;
