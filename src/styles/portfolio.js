56b273f4d7ba5e9a60a78dcdd1e0cdb8c866ebb8
ivan <ivan.kolosinskyi@globallogic.com>
2021-09-14 10:22:09 +0300

    Fix routing

--- src/styles/portfolio.js
+++ src/styles/portfolio.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { breakpoints } from '../../styles/breakpoints';
+import { breakpoints } from './breakpoints';
 
 export const ProjectDetailsContainer = styled.div`
 	padding-left: 16px;
