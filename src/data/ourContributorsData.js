a56b373e02e84fbc472453382d91d50eb0f18b93
Dima Anosov <anosov.work@gmail.com>
2021-12-20 12:48:04 +0200

    translations fix

--- src/data/ourContributorsData.js
+++ src/data/ourContributorsData.js
@@ -51,7 +51,7 @@ const ru = [
 	},
 	{
 		key: 'vladyslavhulin',
-		name: 'Владислав Хулин',
+		name: 'Владислав Гулин',
 		position: 'Дизайнер проектов',
 		img: vladImg,
 	},
@@ -78,7 +78,7 @@ const ua = [
 	},
 	{
 		key: 'vladyslavhulin',
-		name: 'Владислав Хулін',
+		name: 'Владислав Гулін',
 		position: 'Дизайнер проектів',
 		img: vladImg,
 	},
