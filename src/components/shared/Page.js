89172f7a333954b3ee52e3e8263a59903556880b
ivan <ivan.kolosinskyi@globallogic.com>
2022-01-22 19:06:42 +0200

    Fix padding & add leadfeeder

--- src/components/shared/Page.js
+++ src/components/shared/Page.js
@@ -37,6 +37,12 @@ export function Page({ children, pageName }) {
 			</Helmet>
 			{children}
 			<Footer />
+			<script
+				dangerouslySetInnerHTML={{
+					__html:
+						"(function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('Xbp1oaErM9g8EdVj'); ",
+				}}
+			></script>
 		</>
 	);
 }
