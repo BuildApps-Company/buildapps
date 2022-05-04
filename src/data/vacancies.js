d2422ae766c5235de2bca8211c64d1fc1f61011b
Дима Аносов <anosov.work@gmail.com>
2022-05-04 17:10:56 +0000

    Merged in develop (pull request #181)
    Merged in feature/BA-VacancyRemove (pull request #180)
    
    * Merged in feature/BA-VacancyRemove (pull request #180)
    
    remove vacancy page link
    
    * remove vacancy page link

--- src/data/vacancies.js
+++ src/data/vacancies.js
@@ -1,41 +1,4 @@
 const en = {
-	seniorFullstackNetDev: {
-		id: 'seniorFullstackNetDev',
-		title: 'Senior Fullstack .NET Developer',
-		description:
-			'We invite an experienced Senior Fullstack .NET Developer to join the team working on a interesting products for our client from whole world. Our team creates solutions with using ASP.NET Core 3.0+, Azure and microservices architectures.',
-		requirements: ['senior', 'full time', 'remote'],
-		link: '/',
-		pageContent: {
-			location: {
-				remote: true,
-				office: 'Ukraine, Kyiv',
-				mapLink: false,
-			},
-			requirements: [
-				'4-5 years experience of C#;',
-				'Experience with microservice architecture;',
-				'Experience in ASP.NET Core 3.0 web API development;',
-				'Confident knowledge and experience in JS, TS development. Knowledge of one of the leading JS frameworks (React);',
-				'Knowledge of the main approaches to building application architecture, design patterns.',
-				'Experience with Azure;',
-				'Experience in React development;',
-				'Teamwork skills.',
-				'English level - upper intermediate',
-			],
-			tasks: [
-				'Work on projects in the field of e-commerce, fin. and corporate solutions;',
-				'Development of new functionality of the application;',
-				'Change and optimization of the current functionality;',
-				'Bug fixes.',
-			],
-			willBeAPlus: [],
-			conditions: [
-				'Work on new projects using the latest technologies and methodologies',
-				'Flexible working conditions',
-			],
-		},
-	},
 	middleFullStackNetDev: {
 		id: 'middleFullStackNetDev',
 		title: 'Middle Fullstack .NET Developer',
@@ -76,84 +39,9 @@ const en = {
 			],
 		},
 	},
-	middleXamarinFullstackDeveloper: {
-		id: 'middleXamarinFullstackDeveloper',
-		title: 'Strong Junior/Middle Xamarin Full-stack Developer',
-		description:
-			'We invite an experienced Strong Junior or Middle Xamarin Full-stack Developer to join the team working on a interesting products for our client from whole world. Our team creates solutions with using ASP.NET Core 3.0+, Azure and microservices architectures.',
-		requirements: ['mobile', 'junior', 'full time', 'remote'],
-		link: '/',
-		pageContent: {
-			location: {
-				remote: true,
-				office: 'Ukraine, Kyiv',
-				mapLink: false,
-			},
-			requirements: [
-				'Confident knowledge of C#, experience in C# development;',
-				'Experience in Xamarin.Native development (iOS / Android);',
-				'Work experience at least one year',
-				'Knowledge of the main approaches to building application architecture, design patterns.',
-				'Knowledge and ability to apply the principles of SOLID;',
-			],
-			tasks: [
-				'Work on projects in the field of e-commerce, fin. and corporate solutions;',
-				'Development of new functionality of the application;',
-				'Change and optimization of the current functionality;',
-				'Bug fixes.',
-			],
-			willBeAPlus: [
-				'Experience with microservice architecture;',
-				'Experience with Azure;',
-				'Experience with Xamarin.Forms;',
-				'Teamwork skills.',
-			],
-			conditions: [
-				'Work on new projects using the latest technologies and methodologies',
-				'Flexible working conditions',
-			],
-		},
-	},
 };
 
 const ru = {
-	seniorFullstackNetDev: {
-		id: 'seniorFullstackNetDev',
-		title: 'Senior Fullstack .NET Developer',
-		description:
-			'Мы приглашаем опытного Senior Fullstack .NET Developer присоединиться к команде, работающей над интересными продуктами для нашего клиента со всего мира. Наша команда создает решения с использованием архитектур ASP.NET Core 3.0+, Azure и микросервисов.',
-		requirements: ['senior', 'full time', 'remote'],
-		link: '/',
-		pageContent: {
-			location: {
-				remote: true,
-				office: 'Ukraine, Kyiv',
-				mapLink: false,
-			},
-			requirements: [
-				'4-5 лет опыта C#;',
-				'Опыт работы с микросервисной архитектурой;',
-				'Опыт разработки веб-API ASP.NET Core 3.0;',
-				'Уверенные знания и опыт разработки JS, TS. Знание одного из ведущих JS-фреймворков (React);',
-				'Знание основных подходов к построению архитектуры приложений, шаблонов проектирования.',
-				'Опыт работы с Азуром;',
-				'Опыт React-разработки;',
-				'Навык работы в команде.',
-				'Уровень английского - upper intermediate',
-			],
-			tasks: [
-				'Работа над проектами в сфере электронной коммерции, фин. и корпоративные решения;',
-				'Разработка нового функционала приложения;',
-				'Изменение и оптимизация текущего функционала;',
-				'Исправление ошибок.',
-			],
-			willBeAPlus: [],
-			conditions: [
-				'Работа над новыми проектами с использованием новейших технологий и методологий',
-				'Гибкие условия работы',
-			],
-		},
-	},
 	middleFullStackNetDev: {
 		id: 'middleFullStackNetDev',
 		title: 'Middle Fullstack .NET Developer',
@@ -194,84 +82,9 @@ const ru = {
 			],
 		},
 	},
-	middleXamarinFullstackDeveloper: {
-		id: 'middleXamarinFullstackDeveloper',
-		title: 'Strong Junior/Middle Xamarin Full-stack Developer',
-		description:
-			'Мы приглашаем опытного Strong Junior или Middle Xamarin Full-stack Developer присоединиться к команде, работающей над интересными продуктами для нашего клиента со всего мира. Наша команда создает решения с использованием архитектур ASP.NET Core 3.0+, Azure и микросервисов.',
-		requirements: ['mobile', 'junior', 'full time', 'remote'],
-		link: '/',
-		pageContent: {
-			location: {
-				remote: true,
-				office: 'Ukraine, Kyiv',
-				mapLink: false,
-			},
-			requirements: [
-				'Уверенное знание C#, опыт разработки на C#;',
-				'Опыт разработки на Xamarin.Native (iOS/Android);',
-				'Опыт работы не менее года',
-				'Знание основных подходов к построению архитектуры приложений, шаблонов проектирования.',
-				'Знание и умение применять принципы SOLID;',
-			],
-			tasks: [
-				'Работа над проектами в сфере электронной коммерции, фин. и корпоративные решения;',
-				'Разработка нового функционала приложения;',
-				'Изменение и оптимизация текущего функционала;',
-				'Исправление ошибок.',
-			],
-			willBeAPlus: [
-				'Опыт работы с микросервисной архитектурой;',
-				'Опыт работы с Azure;',
-				'Experience with Xamarin.Forms;',
-				'Навык работы в команде.',
-			],
-			conditions: [
-				'Работа над новыми проектами с использованием новейших технологий и методологий',
-				'Гибкие условия работы',
-			],
-		},
-	},
 };
 
 const ua = {
-	seniorFullstackNetDev: {
-		id: 'seniorFullstackNetDev',
-		title: 'Senior Fullstack .NET Developer',
-		description:
-			'Ми запрошуємо досвідченого старшого розробника Fullstack .NET приєднатися до команди, яка працює над цікавими продуктами для нашого клієнта з усього світу. Наша команда створює рішення з використанням архітектури ASP.NET Core 3.0+, Azure та мікросервісів.',
-		requirements: ['senior', 'full time', 'remote'],
-		link: '/',
-		pageContent: {
-			location: {
-				remote: true,
-				office: 'Ukraine, Kyiv',
-				mapLink: false,
-			},
-			requirements: [
-				'4-5 років досвіду роботи на C#;',
-				'Досвід роботи з мікросервісною архітектурою;',
-				'Досвід розробки веб-API ASP.NET Core 3.0;',
-				'Впевнені знання та досвід розробки JS, TS. Знання однієї з провідних JS фреймворків (React);',
-				'Знання основних підходів до побудови архітектури додатків, шаблонів проектування.',
-				'Досвід роботи з Azure;',
-				'Досвід розробки React;',
-				'Навички роботи в команді.',
-				'Рівень англійської мови - upper intermediate',
-			],
-			tasks: [
-				'Робота над проектами у сфері e-commerce, фін. та корпоративні рішення;',
-				'Розробка нового функціоналу програми;',
-				'Зміна та оптимізація поточної функціональності;',
-				'Виправлення помилок.',
-			],
-			willBeAPlus: [],
-			conditions: [
-				'Робота над новими проектами з використанням новітніх технологій і методологій',
-				'Гнучкі умови роботи',
-			],
-		},
-	},
 	middleFullStackNetDev: {
 		id: 'middleFullStackNetDev',
 		title: 'Middle Fullstack .NET Developer',
@@ -304,44 +117,6 @@ const ua = {
 				'Досвід роботи з мікросервісною архітектурою;',
 				'Досвід роботи з Azure;',
 				'Досвід розробки React;',
-				'Навички роботи в команді.',
-			],
-			conditions: [
-				'Робота над новими проектами з використанням новітніх технологій і методологій',
-				'Гнучкі умови роботи',
-			],
-		},
-	},
-	middleXamarinFullstackDeveloper: {
-		id: 'middleXamarinFullstackDeveloper',
-		title: 'Strong Junior/Middle Xamarin Full-stack Developer',
-		description:
-			'Ми запрошуємо досвідченого Strong Junior або Middle Xamarin Full-stack Developer приєднатися до команди, яка працює над цікавими продуктами для нашого клієнта з усього світу. Наша команда створює рішення з використанням архітектури ASP.NET Core 3.0+, Azure та мікросервісів.',
-		requirements: ['mobile', 'junior', 'full time', 'remote'],
-		link: '/',
-		pageContent: {
-			location: {
-				remote: true,
-				office: 'Ukraine, Kyiv',
-				mapLink: false,
-			},
-			requirements: [
-				'Впевнене знання C#, досвід розробки C#;',
-				'Досвід розробки Xamarin.Native (iOS/Android);',
-				'Досвід роботи не менше одного року',
-				'Знання основних підходів до побудови архітектури додатків, шаблонів проектування.',
-				'Знання та вміння застосовувати принципи SOLID;',
-			],
-			tasks: [
-				'Робота над проектами у сфері e-commerce, фін. та корпоративні рішення;',
-				'Розробка нового функціоналу програми;',
-				'Зміна та оптимізація поточної функціональності;',
-				'Виправлення помилок.',
-			],
-			willBeAPlus: [
-				'Досвід роботи з мікросервісною архітектурою;',
-				'Досвід роботи з Azure;',
-				'Досвід роботи з Xamarin.Forms;',
 				'Навички роботи в команді.',
 			],
 			conditions: [
